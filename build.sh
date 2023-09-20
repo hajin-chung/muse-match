99aff8520fe5b643062cfee959a9e07ed4e08890
hajin-chung <hajinchung1@gmail.com>
2023-09-20 14:25:03 +0900

    github actions

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 
 export PROD_PATH="/opt/musematch"
 
+# pull latest version
+git pull
+
 # copy template files
 cp -r views "$PROD_PATH/"
 
 # build server binary
 go build -o "$PROD_PATH/server"
 
-# reload server daemon
-sudo systemctl reload musematch
+# restart server daemon
+sudo systemctl restart musematch
