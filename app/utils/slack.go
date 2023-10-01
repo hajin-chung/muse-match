0dfcdb2d90b9b7e3b3c7133337d0f553c1b4f7b2
hajin-chung <hajinchung1@gmail.com>
2023-10-01 23:02:52 +0900

    log instead of sending to slack on dev mode

--- app/utils/slack.go
+++ app/utils/slack.go
@@ -3,12 +3,16 @@ package utils
 import (
 	"bytes"
 	"encoding/json"
+	"log"
 	"musematch/app/globals"
 	"net/http"
 )
 
 func SlackSendMessage(message string) error {
-	return nil
+	if globals.Env.MODE != "PROD" {
+		log.Println(message)
+		return nil
+	}
 	body := map[string]string{"text": message}
 	json, _ := json.Marshal(body)
 	_, err := http.Post(globals.Env.SLACK_WEBHOOK, "application/json", bytes.NewBuffer(json))
